9f1fc43a6e429ffebe9f5fb33e31fb7eb5922075
mehrab <mehrabul.ferdous@gmail.com>
2025-09-28 21:53:57 +0600

    added junit

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -15,4 +15,6 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+//    testImplementation ("org.junit:junit-bom:5.10.0")
+    testImplementation ("org.junit.jupiter:junit-jupiter")
 }
---
+++ user-service/src/test/java/UserServiceTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+    @Test
+    public void userTest() {
+        assert(true);
+        System.out.println("User Service Test");
+    }
+}
